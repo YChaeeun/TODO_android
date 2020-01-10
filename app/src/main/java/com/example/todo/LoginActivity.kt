103e22423d5d4bafacfbb297b7b6ac464750bee6
YChaeeun <se_same5315@naver.com>
2020-01-10 17:35:27 +0900

    feat-#2 [login] 기본 ui 레이아웃
    - 메일 주소, 비밀번호, 회원가입, 비밀번호찾기, 로그인
    - 키보드 올라올 때 아래에 있는 버튼들도 올라오게 설정

---
+++ app/src/main/java/com/example/todo/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.todo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.layout_login_container, LoginFragment() )
+            .commit()
+    }
+
+}
---
+++ app/src/main/java/com/example/todo/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.todo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_login_main, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
